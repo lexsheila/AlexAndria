f22cabc19a6968739aa4a5245a2ee01ed6f9c3d5
lexsheila <alexandriasheila@gmail.com>
2014-08-06 16:46:59 -0400

    better look

--- blog/models.py
+++ blog/models.py
@@ -9,21 +9,18 @@ class Blog(models.Model):
     body = models.TextField()
     posted = models.DateTimeField(db_index=True, auto_now_add=True)
     category = models.ForeignKey('blog.Category', null=True)
-
     def __unicode__(self):
         return '%s' % self.title
-
     @permalink
     def get_absolute_url(self):
         return ('view_blog_post', None, { 'slug': self.slug })
 
+
 class Category(models.Model):
     title = models.CharField(max_length=100, db_index=True)
     slug = models.SlugField(max_length=100, db_index=True)
-
     def __unicode__(self):
         return '%s' % self.title
-
     @permalink
     def get_absolute_url(self):
         return ('view_blog_category', None, { 'slug': self.slug })
--- blog/views.py
+++ blog/views.py
@@ -31,7 +31,6 @@ def post_new(request):
 
     form = PostForm()
     if request.method == 'POST':
-
         form = PostForm(request.POST)
         if form.is_valid():
             form.save()
